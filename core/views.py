fd7cfbfc784c833bdd2fc2cc172edeb984a9bd7b
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2025-10-13 21:46:23 -0300

    estrutura base e estilização

---
+++ core/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+def home_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'core/base.html')
