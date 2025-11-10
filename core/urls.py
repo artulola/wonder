685ed25b2e60b781e6002608982065dba3afac8a
João Neto <112356355+JoaoRoquePNeto@users.noreply.github.com>
2025-11-10 15:26:57 -0300

    Revert "Cria a Tela Perfil prestador"

--- core/urls.py
+++ core/urls.py
@@ -18,28 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_cliente_view, name='home-cliente'),
-    path('busca/', views.search, name= 'search'),
-    path('busca/resultado-busca/', views.search_results_view, name='resultado-busca'),
-    path('login/', views.login_view, name='login' ),
-    path('cadastro/', views.cadastro_view, name='cadastro'),
-    path('opcoes-cadastro/', views.register_options_view, name='opcoes-cadastro'),
-    path('cidade/', views.cidade_view, name='cidade'),
-    path('perfil-usuario/', views.perfil_usuario_view, name='perfil-usuario'),
-    path('detalhe-estabelecimento/', views.detalhe_estabelecimento_view, name='detalhe-estabelecimento'),
-    path('agendamentos/', views.agendamento_view, name='agendamento'),
-    path('agendamentos/finalizados/', views.agendamentos_finalizados_view, name='agendamentos-finalizados'),
-    path('cadastro-prestador/', views.cadastro_prestador_view, name='cadastro-prestador'),
-    path('solicitacoes/', views.solicitacoes_view, name='solicitacoes'),
-    path('home_prestador/', views.home_prestador_view, name='home_prestador'),
-    path('solicitacoes/detalhes-solicitacoes/', views.detalhes_solicitacoes_view, name='detalhes-solicitacoes'),
-    path('categorias/', views.categorias_view, name='categorias'),
-    path('home_finalizados/', views.home_finalizados_view, name='home_finalizados'),
-    path('home_cancelados/', views.home_cancelados_view, name='home_cancelados'),
-    path('perfil_prestador/', views.perfil_prestador_view, name='perfil_prestador'),
-    
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
