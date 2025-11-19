00f0901cada2b58e3d388ecf920ec2cd38dc4e8e
João Roque Pereira Neto <joaoroquepneto@gmail.com>
2025-11-19 15:32:23 -0300

    Paginacao de horarios

--- core/urls.py
+++ core/urls.py
@@ -34,5 +34,7 @@ urlpatterns = [
     path('prestador/home/', prestador_views.prestador_home_view, name='prestador-home'),
     path('cliente/estabelecimento_horarios/', cliente_views.cliente_estabelecimento_horarios_view, name='cliente-estabelecimento-horarios'),
     path('cliente/estabelecimento-horarios/calendario/', cliente_views.cliente_estabelecimento_horarios_calendario_view, name='cliente-estabelecimento-horarios-calendario'),
+    path('cliente/estabelecimento/horarios/',cliente_views.cliente_estabelecimento_horarios_view, name='cliente-estabelecimento-horarios'),
  
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- core/views/cliente_views.py
+++ core/views/cliente_views.py
@@ -33,3 +33,6 @@ def cliente_estabelecimento_horarios_view(request: HttpRequest) -> HttpResponse:
 
 def cliente_estabelecimento_horarios_calendario_view(request: HttpRequest) -> HttpResponse:
     return render(request, 'core/cliente/estabelecimento_calendario.html')
+
+def cliente_estabelecimento_horarios_view(request):
+    return render(request, 'core/cliente/horarios-disponiveis.html')
