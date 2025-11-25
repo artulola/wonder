ef0d4f23c2b5117870102d9a2bea1927d85a69b7
llwkascarvalho <lwkascarvalho1@gmail.com>
2025-11-25 17:50:20 -0300

    feat: Configurações dos Models e app Accounts

---
+++ accounts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Cliente)
+admin.site.register(Categoria)
+admin.site.register(Servico)
+admin.site.register(Agendamento)
+admin.site.register(Avaliacao)
+admin.site.register(Prestador)
+admin.site.register(CustomUser)
