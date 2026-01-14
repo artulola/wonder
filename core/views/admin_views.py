1b4cde00eebc9917cb2b2d0952f1adeabc10ca73
llwkascarvalho <lwkascarvalho1@gmail.com>
2026-01-13 21:32:27 -0300

    feat: Protege views de admin com @admin_required

--- core/views/admin_views.py
+++ core/views/admin_views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from accounts.models import Prestador
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
+from accounts.decorators import admin_required
 
-
+@admin_required
 def admin_solicitacoes_view(request: HttpRequest) -> HttpResponse:
     pendentes = Prestador.objects.filter(status=Prestador.StatusPrestador.PENDENTE)
     return render(request, 'core/admin/solicitacoes.html', {'pendentes': pendentes})
 
-
+@admin_required
 def admin_detalhes_solicitacoes_view(request: HttpRequest, prestador_id: int) -> HttpResponse:
     prestador = get_object_or_404(Prestador, id=prestador_id)
 
@@ -28,5 +30,6 @@ def admin_detalhes_solicitacoes_view(request: HttpRequest, prestador_id: int) ->
 
     return render(request, 'core/admin/detalhes_solicitacao.html', {'prestador': prestador})
 
+@admin_required
 def admin_categorias_view(request: HttpRequest) -> HttpResponse:
     return render(request, 'core/admin/categorias.html')
